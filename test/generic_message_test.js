66bb2d4648c5215270d08d88d97477bf8480e11b
Antares1980 <ja.pv@icloud.com>
2021-09-01 16:44:25 +0200

    adding regular expression to the test and gitignore files

--- test/generic_message_test.js
+++ test/generic_message_test.js
@@ -7,7 +7,7 @@ describe("generic_message", function() {
     describe("getGenericMessage", function() {
         it("Should return Hello entity. It's ", function() {
             let message = generic_messages.getGenericMessage();
-            let comparison = message.includes("Hello entity. It's");
+            let comparison = message.match('Hello entity. It\'s\\s\\d{1,2}\\sh\\s\\d{1,2}\\sm');
             comparison.should.be.true;
         });
     })
