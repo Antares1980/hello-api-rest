4e04f444a847420de3483682e7ea5cf9940318f9
Antares1980 <ja.pv@icloud.com>
2021-09-01 13:54:07 +0200

    Adding pre-commit hooks

--- routes/generic.js
+++ routes/generic.js
@@ -1,4 +1,7 @@
 exports.getGenericMessage = () => {
+    if (false) {
+        console.log("not compared correctly");
+    }
     return "Hello entity. It's " + new Date().getHours() + " h " + new Date().getMinutes() + " m";
     ;
   };
